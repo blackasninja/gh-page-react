1a1975a6d7afa72810a654840f00aab35a0cc32a
Ashwin Patel <ash_winp@hotmail.com>
2021-08-31 21:33:31 +1200

    Add routing animation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import Button from 'react-bootstrap/Button';
-import Header from './components/Header/Header'
-import { Route, Switch, Link } from "react-router-dom";
-import Home from './pages/Home';
-import Skills from './pages/Skills';
-import Projects from './pages/Projects';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header/Header";
+import { Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Skills from "./pages/Skills";
+import Projects from "./pages/Projects";
+import { AnimatedSwitch } from 'react-router-transition';
 
 function App() {
+
   return (
     <div className="App">
       <Header></Header>
-      <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/projects">
-              <Projects />
-            </Route>
-            <Route path="/skills">
-              <Skills />
-            </Route>
-          </Switch>
+      <AnimatedSwitch
+      atEnter={{ opacity: 0 }}
+      atLeave={{ opacity: 0 }}
+      atActive={{ opacity: 1 }}
+      className="switch-wrapper"
+    >
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/projects">
+          <Projects />
+        </Route>
+        <Route path="/skills">
+          <Skills />
+        </Route>
+      </AnimatedSwitch>
     </div>
   );
 }
